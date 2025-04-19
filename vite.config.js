a24428ac96a8583cc6f0e000997903fe5586976c
C4rbo <alessiocar02.ac@gmail.com>
2025-04-19 20:01:45 +0200

    a

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/C4rb0.github.io/',
+  base: '/c4rb0.github.io/',
 })
